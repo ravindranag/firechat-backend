5e3bd358786ffedace294d3177b5aefebea0ed7c
ravindranag <ravindranag52@gmail.com>
2023-05-28 12:16:01 +0530

    feat: api status

--- src/app/index.js
+++ src/app/index.js
@@ -2,8 +2,25 @@ import { Router } from "express";
 import { roomRouter } from "./room/index.js";
 import { userRouter } from "./user/index.js";
 import { friendRouter } from "./friend/index.js";
+import db from "../lib/prisma/init.js";
 
 const appRouter = Router()
+
+appRouter.get('/status', async (req, res, next) => {
+	try {
+		const metrics = await db.$metrics.json()
+		res.json({
+			message: 'Server running',
+			connection: metrics.counters[2].value
+		})
+	}
+	catch(err) {
+		console.log(err)
+		res.status(500).json({
+			message: 'Server running but db not'
+		})
+	}
+})
 
 appRouter.use('/room', roomRouter)
 appRouter.use('/user', userRouter)
